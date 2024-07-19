42fc0bfcab79d9d7a81e1bf6b9ef7be1302a2e09
Kubota <74347995+KubotaDaiki@users.noreply.github.com>
2024-07-19 15:08:41 +0900

    feat: viteの導入 (#4)

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { crx, defineManifest } from "@crxjs/vite-plugin";
+import { defineConfig } from "vite";
+
+const manifest = defineManifest({
+  name: "Hide X Cursor",
+  version: "0.1.1",
+  manifest_version: 3,
+  icons: {
+    "16": "icons/icon.png",
+    "48": "icons/icon.png",
+    "128": "icons/icon.png",
+  },
+  description:
+    "X（旧Twitter）の動画上で一定時間マウスを動かしていないとき、カーソルを非表示にします。",
+  content_scripts: [
+    {
+      matches: ["https://x.com/*"],
+      js: ["src/content-script.js"],
+    },
+  ],
+});
+
+export default defineConfig({
+  plugins: [crx({ manifest })],
+});
