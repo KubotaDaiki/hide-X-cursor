1b840a909d5c7903130270f9ca8f559865ae2b39
Kubota <kubotadaiki0654@gmail.com>
2024-07-19 15:18:20 +0900

    feat: typescriptの導入

--- src/content-script.ts
+++ src/content-script.ts
@@ -9,11 +9,11 @@ window.addEventListener("mousemove", function () {
   }, 2000);
 });
 
-function changeCursorShape(shape) {
+function changeCursorShape(shape: string) {
   const videoControlClass =
     "css-175oi2r r-1p0dtai r-1d2f490 r-u8s1d r-zchlnj r-ipm5af r-1loqt21";
   const videoControlElements =
-    document.getElementsByClassName(videoControlClass);
+    document.getElementsByClassName(videoControlClass) as HTMLCollectionOf<HTMLElement>;
   [...videoControlElements].forEach((video) => {
     video.style.cursor = shape;
   });
